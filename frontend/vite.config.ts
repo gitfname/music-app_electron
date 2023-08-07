37e3a23b37b0531e105a9b016d7cfc4c141e67c7
gitfname <newamailnam@gmail.com>
2023-08-08 00:16:22 +0430

    i just fixed my first major fucking-up in this project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_PUBLIC_MODE === "development" ? "/" : "./",
-  build: {
-    emptyOutDir: true,
-    outDir: "../app-front"
-  },
   plugins: [react()],
 })
